d1a1d5fc3d925c7ad84d41ee7bc3967a3b9307f6
Debasish <debasish16.nayak@gmail.com>
2026-01-22 12:58:40 +0530

    add daily task planner and motivational quote page

--- script.js
+++ script.js
@@ -1,104 +1,150 @@
-function openFeatures(){
-    let elems = document.querySelectorAll('.elem');
-let fullElems = document.querySelectorAll('.fullElems');
-let back=document.querySelectorAll('.fullElems .back')
+//! navigation to other page  functionalisties
+function openFeatures() {
+  let elems = document.querySelectorAll(".elem");
+  let fullElems = document.querySelectorAll(".fullElems");
+  let back = document.querySelectorAll(".fullElems .back");
 
-//? to open indivisual pages
-elems.forEach((elem)=>{
-    elem.addEventListener('click',(()=>{
-       fullElems[elem.id].style.display='block'
-    }))
-})
+  //? to open indivisual pages
+  elems.forEach((elem) => {
+    elem.addEventListener("click", () => {
+      fullElems[elem.id].style.display = "block";
+    });
+  });
 
-//?back btn logic
-back.forEach((elem)=>{
-
-   elem.addEventListener('click',((backelem)=>{
-        console.log(elem.id);
-         fullElems[elem.id].style.display='none'
-   }))
-})
+  //?back btn logic
+  back.forEach((elem) => {
+    elem.addEventListener("click", (backelem) => {
+      console.log(elem.id);
+      fullElems[elem.id].style.display = "none";
+    });
+  });
 }
-openFeatures()
+openFeatures();
 
-const checkBox=document.querySelector('.todoForm #imp input')
+//! to do list functionalisties
+function todoList() {
+  const checkBox = document.querySelector(".todoForm #imp input");
 
-console.log(checkBox);
-checkBox.addEventListener('changed',()=>{
-     currentTask[index].imp = checkBox.checked;
-})
+  console.log(checkBox);
+  checkBox.addEventListener("change", () => {
+    currentTask[index].imp = checkBox.checked;
+  });
 
-const taskList=document.querySelector('.taskList')
+  const taskList = document.querySelector(".taskList");
 
+  var currentTask = [];
+  function storage() {
+      const data = localStorage.getItem("currentTask");
+    if (!data) {
+      console.log("list is empty");
+        taskList.innerText = "Need to add task";
+    } else {
+      currentTask = JSON.parse(data);
+      console.log("full");
+    
+    }
+  }
+  storage();
 
+  const form = document.querySelector(".todoForm");
+  const input = document.querySelector(".todoForm input");
+  const textarea = document.querySelector(".todoForm textarea");
+  const imp = document.querySelector(".imp");
 
-var currentTask=[]
-function storage(){
-    if(localStorage.getItem('currentTask')==[]){
-    console.log('list is empty');
-   
-}else{
-     currentTask=JSON.parse(localStorage.getItem('currentTask'))
-    console.log('full');
-    taskList.innerText='Need to add task'
-}
-}
-storage()
-
-const form=document.querySelector('.todoForm')
-const input=document.querySelector('.todoForm input')
-const textarea=document.querySelector('.todoForm textarea')
-const imp=document.querySelector('.imp')
-
-
-
-
-
-form.addEventListener('submit',(e)=>{
+  form.addEventListener("submit", (e) => {
     e.preventDefault();
     console.log(input.value, textarea.value);
-        console.log(currentTask);
-        currentTask.push({
-            task:input.value,
-            des:textarea.value,
-            imp:checkBox.checked,
-        })
-        localStorage.setItem('currentTask', JSON.stringify(currentTask))
-        renderTaxk()
+    console.log(currentTask);
+    currentTask.push({
+      task: input.value,
+      des: textarea.value,
+      imp: checkBox.checked,
+    });
 
-})
+    localStorage.setItem("currentTask", JSON.stringify(currentTask));
+    renderTaxk();
+    //     checkBox.checked=false;
+    // input.value=''
+    // textarea.value=''
 
-const renderTaxk=()=>{
-    
-    
-    let sum=''
-currentTask.forEach((ele,idx)=>{
+    //or
 
-    console.log('hello');
-    console.log(ele.task);
-    sum+=` <div class="taskInfo">
+    form.reset();
+  });
+
+  const renderTaxk = () => {
+    let sum = "";
+    currentTask.forEach((ele, idx) => {
+      console.log("hello");
+      console.log(ele.task);
+      sum += ` <div class="taskInfo">
         <div>
             <h4>${ele.task}</h4>
         <p style="margin-bottom: 20px;">${ele.des}</p>
-      <span class="imp" style="display:${ele.imp ? 'block' : 'none'}">
+      <span class="imp" style="display:${ele.imp ? "block" : "none"}">
             important!
           </span>
         </div>
         <div ><button class="status" id=${idx}>Mark as Completed</button></div>
-      </div>`
-})
-taskList.innerHTML=sum
+      </div>`;
+    });
+    taskList.innerHTML = sum;
 
+    //? delete functionality-----❌
+
+    document.querySelectorAll(".taskInfo button").forEach((btn) => {
+      btn.addEventListener("click", () => {
+        currentTask.splice(btn.id, 1);
+        renderTaxk();
+      });
+    });
+  };
+  renderTaxk();
 }
-renderTaxk()
+
+todoList();
 
 
+//! daily planner
 
-taskList.addEventListener('click', (e) => {
-  if (e.target.classList.contains('status')) {
-    // const index = e.target.id;
-    currentTask.splice(e.target.id, 1);
-    localStorage.setItem('currentTask', JSON.stringify(currentTask));
-    renderTaxk();
-  }
-});
+function dailyPlanner(){
+  const PlanDay=document.querySelector('.PlanDay')
+
+let hour=Array.from({length:18},(ele,idx)=>{
+    return `${6+idx }:00 - ${7+idx}:00`;
+})
+let hourSum=''
+let data=JSON.parse(localStorage.getItem('dailyTask'))||{}
+    console.log(data);
+hour.forEach((elem,idx)=>{
+    console.log(elem);
+   var savedData=data[idx]||''
+    hourSum+=` <div class="pllaner-time">
+                       <p>${elem}</p>
+                       <input id=${idx} type="text" placeholder="...." value=${savedData}>
+                    
+                </div>`
+})
+
+
+PlanDay.innerHTML=hourSum
+var dailyTask={}
+let planner_time=document.querySelectorAll('.pllaner-time input')
+planner_time.forEach((ele)=>{
+    ele.addEventListener('change',()=>{
+        console.log(ele.id);
+       dailyTask[ele.id]=ele.value
+       localStorage.setItem('dailyTask',JSON.stringify(dailyTask))
+    })
+})
+}
+
+dailyPlanner()
+
+
+async function randomQuotes(){
+  let res=await fetch('https://zenquotes.io/api/quotes')
+  let data=await res.json()
+  console.log(data);
+}
+randomQuotes()
